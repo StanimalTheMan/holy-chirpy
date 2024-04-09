e53f07daeebf1c3419000f44d409ef45dbbde831
Stan Choi <sjc591@nyu.edu>
2024-04-09 09:26:45 -0400

    Build and run server that bounds to port 8080, responds with 404 for now

--- main.go
+++ main.go
@@ -1,56 +1,39 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
 	"net/http"
-	"os"
-
-	"github.com/joho/godotenv"
 )
 
 func main() {
-	// OpenAI moderation endpoint
-	url := "https://api.openai.com/v1/moderations"
+	// Create a new http.ServeMux
+	mux := http.NewServeMux()
 
-	jsonStr := []byte(`{
-		"input": "You have a big face.  Get plastic surgery"
-	}`)
+	// Wrap mux in custom middleware that adds CORS headers to response
+	corsMux := middlewareCors(mux)
 
-	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
+	// Create a new http.Server and use corsMux on handler
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: corsMux,
+	}
+
+	// Start HTTP server
+	err := server.ListenAndServe()
 	if err != nil {
-		fmt.Println("Error creating request", err)
-		return
+		fmt.Println("Error starting server:", err)
 	}
+}
 
-	req.Header.Set("Content-Type", "application/json")
-
-	// Load environment variables from the .env file
-	if err := godotenv.Load(".env"); err != nil {
-		fmt.Println("Error loading .env file:", err)
-		return
-	}
-
-	// Get value of "OPENAI_API_KEY" environment variable from .env file
-	apiKey := os.Getenv("OPENAI_API_KEY")
-
-	req.Header.Set("Authorization", fmt.Sprintf("Bearer %v", apiKey))
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		fmt.Println("Error sending request:", err)
-		return
-	}
-	defer resp.Body.Close()
-
-	fmt.Println("Response Status:", resp.Status)
-
-	body := make([]byte, 1024)
-	_, err = resp.Body.Read(body)
-	if err != nil {
-		fmt.Println("Error reading response body:", err)
-		return
-	}
-	fmt.Println("Response Body:", string(body))
+func middlewareCors(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "*")
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
 }
